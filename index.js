8a2493e8c9f7210a3821f8801a27aa170cb1519b
William Pittman <wpittman1617@gmail.com>
2022-07-08 21:35:20 -0400

    commit tree ffd2dd6a91ef1be3563094ecd560a5721a522459

---
+++ index.js
@@ -0,0 +1,42 @@
+let a = 5
+let b = 6
+
+function add(a,b) {
+    // adds (a + b)
+    return a + b;
+  }
+
+function subtract(a,b) {
+  // subtracts (a - b)
+  return a - b;
+}
+
+function multiply(a,b) {
+  // multiplies (a * b)
+return a * b;
+}
+
+function divide(a,b) {
+// divides (a * b)
+return a / b;
+}
+
+//increment
+let q  = 928;
+function increment(q) {
+return ++q;
+}
+
+// decrement
+let p = 61;
+function decrement(p) {
+return --p;
+}
+
+
+// parses an integer
+var n = parseInt("7",10);
+function makeInt(n) {
+  return n;
+ }
+
